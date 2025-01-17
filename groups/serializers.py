1e1cb5c78ddc9a98268dea78cfb401e7377efc78
Crooter777 <obstinate.crooter@gmail.com>
2025-01-17 09:56:12 +0600

    add group branch to serializer

--- groups/serializers.py
+++ groups/serializers.py
@@ -6,4 +6,4 @@ from .models import Group
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ['name', 'start_date', 'end_date', 'id']
+        fields = ['name', 'start_date', 'end_date', 'id', 'branch']
